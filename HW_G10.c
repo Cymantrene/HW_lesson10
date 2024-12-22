46bfcdf095d6a94cb9bd28a5c60b0abd99d4aa03
Cymantrene <rengenhik@gmail.com>
2024-12-22 12:41:23 +0300

    Add files via upload

--- HW_G10.c
+++ HW_G10.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 #include <string.h>
-
-
+//#include <locale.h>
 int main()
 {
+//system("chcp 1251");
+//setlocale(LC_ALL, "Rus");
+char * input_fn = "input.txt";
+char * output_fn = "output.txt";
+char s[10000];
+char res[10000];
+int i, j=0, index_max, max, count, len;
 
-    char * input_fn = "input.txt";
-    char * output_fn = "output.txt";
-    char res[1000] = {'\0'};
-    char str[1000];
-    char buf[1000];
-    char c;
-    int i=0, j=0;
-    FILE *fp_in = fopen(input_fn, "r");
-    FILE *fp_out = fopen(output_fn, "w");
-    fgets (str, 1000, fp_in);
-    while (str[i] != '\0')
+ FILE *fp_in = fopen(input_fn, "r");
+ FILE *fp_out = fopen(output_fn, "w");
+
+fgets (s, 10000, fp_in);
+len = strlen(s);
+max = 0;
+index_max = 0;
+count = 0;
+for (i=0; i<len; i++)
+    if (s[i] != ' ') count++;
+    else
     {
-        buf[i]= str[i];
-        i++;
-    }
-    j = 0;
-    i++;
-     for (i=0; i< strlen(buf); i++)
-    {
-        c = buf[i];
-        if (strchr(res,c) == NULL && c != ' ')
+        if (count > max)
         {
-            res[j] = c;
-            ++j; // += 1;
+            max = count;
+            index_max = i - count;
         }
+        count = 0;
     }
+
+if (count > max) {  // Проверка последнего слова
+    max = count;
+    index_max = i - count;
+}
+max += index_max; // Сложение, совмещённое с присваиванием
+//printf("\nThe long word:\n");
+for (i=index_max; i<max; i++) //Создание цикла подсчета
+    //putchar(s[i]); //Вывод символов по индексу
+   {
+       res[j++]= s[i];
+   }
     res[j] = '\0';
-    puts(res);
-    fprintf(fp_out, "%s", res);
+   fprintf(fp_out, "%s", res);
+
     fclose(fp_in);
     fclose(fp_out);
     return 0;
